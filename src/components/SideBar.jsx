6afde2350126dcb0f33dfa0a3edce49d7ebd1c6a
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-04-24 18:03:47 +0100

    Reafactored the SideBar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
@@ -7,26 +8,15 @@ const StyledSideBar = styled.aside`
   display: flex;
   flex-direction: column;
   background-color: ${({ theme }) => theme.sidebarBackground};
-
-  ul {
-    padding: 0;
-    margin-top: 20px;
-    list-style-type: none;
-
-    li {
-      color: ${({ theme }) => theme.sidebarText};
-      &:hover {
-        background-color: ${({ theme }) => theme.sidebarHover};
-        cursor: pointer;
-      }
-      .active {
-        background-color: ${({ theme }) => theme.sidebarActiveLink};
-      }
-    }
-  }
 `;
 
-const StyledSidebarLink = styled(NavLink)`
+const StyledSideBarMenu = styled.ul`
+  padding: 0;
+  margin-top: 20px;
+  list-style-type: none;
+`;
+
+const StyledSideBarLink = styled(NavLink)`
   text-decoration: none;
   &:focus,
   &:hover,
@@ -38,23 +28,47 @@ const StyledSidebarLink = styled(NavLink)`
   }
   padding: 20px 20px 20px 40px;
   display: block;
+  cursor: ${({ header }) => (header ? 'default' : 'pointer')};
 `;
+
+const StyledSidebarMenuLink = styled.li`
+  color: ${({ theme }) => theme.sidebarText};
+  ${({ header, theme }) =>
+    !header && `&:hover {background-color: ${theme.sidebarHover};}`}
+  .active {
+    background-color: ${({ theme }) => theme.sidebarActiveLink};
+  }
+`;
+
+const SidebarMenuLink = props => {
+  const { header, text, ...rest } = props;
+  return (
+    <StyledSidebarMenuLink header={header}>
+      <StyledSideBarLink header={header} {...rest}>
+        {header ? <h3>{text}</h3> : text}
+      </StyledSideBarLink>
+    </StyledSidebarMenuLink>
+  );
+};
 
 const SideBar = () => {
   return (
     <StyledSideBar>
-      <ul>
-        <li>
-          <StyledSidebarLink to="/" as={Link}>
-            <h3>Skills Test</h3>
-          </StyledSidebarLink>
-        </li>
-        <li>
-          <StyledSidebarLink to="/login">Login</StyledSidebarLink>
-        </li>
-      </ul>
+      <StyledSideBarMenu>
+        <SidebarMenuLink header to="/" as={Link} text="Skills Matrix" />
+        <SidebarMenuLink to="/login" text="Login" />
+      </StyledSideBarMenu>
     </StyledSideBar>
   );
+};
+
+SidebarMenuLink.defaultProps = {
+  header: false,
+};
+
+SidebarMenuLink.propTypes = {
+  header: PropTypes.bool,
+  text: PropTypes.string.isRequired,
 };
 
 export default SideBar;
