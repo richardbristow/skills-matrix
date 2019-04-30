6afadf15abf25f23b198d5c15edf796ffed5d8e0
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-04-30 21:34:57 +0100

    Refactored Login component to use hooks

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Card, Alert, InputGroup } from 'react-bootstrap';
 import { Auth } from 'aws-amplify';
 import { AtSign, Lock } from 'react-feather';
@@ -12,24 +12,20 @@ const StyledLogin = styled(StyledMain)`
   }
 `;
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '', password: '', validated: false, authError: null };
+const Login = () => {
+  const [values, setValues] = useState({ email: '', password: '' });
+  const [validated, setValidated] = useState(false);
+  const [authError, setAuthError] = useState(null);
+  const { email, password } = values;
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.checkForEmptyForm = this.checkForEmptyForm.bind(this);
-  }
-
-  handleInputChange({ target }) {
+  const handleInputChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value, authError: null });
-  }
+    setValues({ ...values, [name]: value });
+    setAuthError(null);
+  };
 
-  async handleSubmit(event) {
+  const handleSubmit = async event => {
     event.preventDefault();
-    const { email, password } = this.state;
     const form = event.currentTarget;
 
     if (form.checkValidity() === false) {
@@ -40,85 +36,81 @@ class Login extends Component {
         // eslint-disable-next-line no-alert
         alert('Logged in');
       } catch (err) {
-        this.setState({ authError: err.message });
+        setAuthError(err.message);
       }
     }
-    this.setState({ validated: true });
-  }
+    setValidated(true);
+  };
 
-  checkForEmptyForm() {
-    const { email, password } = this.state;
+  const checkForEmptyForm = () => {
     return !(email.length > 0 && password.length > 0);
-  }
+  };
 
-  render() {
-    const { email, password, validated, authError } = this.state;
-    return (
-      <StyledLogin centre>
-        <Card>
-          <Card.Header as="h3">Login</Card.Header>
-          <Card.Body>
-            <Form validated={validated} noValidate onSubmit={this.handleSubmit}>
-              <Form.Group controlId="email">
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>
-                      <AtSign size={16} />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    value={email}
-                    autoFocus
-                    onChange={this.handleInputChange}
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a valid email address.
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-              <Form.Group controlId="password">
-                <InputGroup>
-                  <InputGroup.Prepend>
-                    <InputGroup.Text>
-                      <Lock size={16} />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <Form.Control
-                    value={password}
-                    onChange={this.handleInputChange}
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a password.
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-              {authError && (
-                <Alert variant="danger">
-                  <strong>Error:</strong> {authError}
-                </Alert>
-              )}
-              <Button
-                variant="outline-primary"
-                disabled={this.checkForEmptyForm()}
-                type="submit"
-                block
-              >
-                Login
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </StyledLogin>
-    );
-  }
-}
+  return (
+    <StyledLogin centre>
+      <Card>
+        <Card.Header as="h3">Login</Card.Header>
+        <Card.Body>
+          <Form validated={validated} noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>
+                    <AtSign size={16} />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  value={email}
+                  autoFocus
+                  onChange={handleInputChange}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid email address.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>
+                    <Lock size={16} />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  value={password}
+                  onChange={handleInputChange}
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a password.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+            {authError && (
+              <Alert variant="danger">
+                <strong>Error:</strong> {authError}
+              </Alert>
+            )}
+            <Button
+              variant="outline-primary"
+              disabled={checkForEmptyForm()}
+              type="submit"
+              block
+            >
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </StyledLogin>
+  );
+};
 
 export default Login;
