17d20f77e997c388a978ffe0da0d902442d668ad
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-05-27 11:39:04 +0100

    Added handlers for all CRUD operation on skillsList

--- backend/lambda/skillsList.js
+++ backend/lambda/skillsList.js
@@ -4,38 +4,26 @@ const DynamoDB = require('aws-sdk/clients/dynamodb');
 
 const docClient = new DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-const params = {
-  TableName: process.env.TABLENAME,
-};
-
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
 };
 
-// const skillsTableGet = args =>
-//   new Promise((resolve, reject) => {
-//     docClient.scan(args, (error, data) => {
-//       if (error) {
-//         return reject(error);
-//       }
-//       return resolve(data);
-//     });
-//   });
+const skillsListGet = async () => {
+  const params = {
+    TableName: process.env.TABLENAME,
+  };
 
-const skillsTableGet = args =>
-  docClient
-    .scan(args, (error, data) => {
-      if (error) {
-        return error;
-      }
-      return data;
-    })
-    .promise();
-
-exports.skillsList = async () => {
   try {
-    const dynamoResponse = await skillsTableGet(params);
+    const dynamoResponse = await docClient
+      .scan(params, (error, data) => {
+        if (error) {
+          return error;
+        }
+        return data;
+      })
+      .promise();
+
     console.log('dynamoResponse', dynamoResponse);
     console.log(`Success getting ${process.env.TABLENAME}`);
 
@@ -49,4 +37,111 @@ exports.skillsList = async () => {
   } catch (error) {
     throw new Error(error);
   }
+};
+
+const skillsListAdd = async event => {
+  const body = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLENAME,
+    Item: { ...body },
+  };
+
+  console.log('params :', params);
+
+  try {
+    const dynamoResponse = await docClient
+      .put(params, (error, data) => {
+        if (error) {
+          return error;
+        }
+        return data;
+      })
+      .promise();
+
+    const response = {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify({ ...dynamoResponse }),
+    };
+
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const skillsListEdit = async event => {
+  const body = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLENAME,
+    Key: { ...event.pathParameters },
+    UpdateExpression: 'set skillDescription = :d',
+    ExpressionAttributeValues: {
+      ':d': body.skillDescription,
+    },
+  };
+
+  try {
+    const dynamoResponse = await docClient
+      .update(params, (error, data) => {
+        if (error) {
+          return error;
+        }
+        return data;
+      })
+      .promise();
+
+    const response = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ ...dynamoResponse }),
+    };
+
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const skillsListDelete = async event => {
+  const params = {
+    TableName: process.env.TABLENAME,
+    Key: { ...event.pathParameters },
+  };
+
+  console.log('params :', params);
+  try {
+    const dynamoResponse = await docClient
+      .delete(params, (error, data) => {
+        if (error) {
+          return error;
+        }
+        return data;
+      })
+      .promise();
+
+    const response = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ ...dynamoResponse }),
+    };
+
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const handlers = {
+  GET: skillsListGet,
+  POST: skillsListAdd,
+  PATCH: skillsListEdit,
+  DELETE: skillsListDelete,
+};
+
+exports.skillsList = event => {
+  const { httpMethod } = event;
+  return handlers[httpMethod](event);
 };
