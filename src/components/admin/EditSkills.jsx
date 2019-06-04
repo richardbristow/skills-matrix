b8388c6b44deaac2f5ec37de6171234185324d86
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-06-04 09:20:15 +0100

    Added pre filled modals on edit

--- src/components/admin/EditSkills.jsx
+++ src/components/admin/EditSkills.jsx
@@ -30,64 +30,151 @@ const StyledButton = styled.button`
   background-color: transparent;
 `;
 
-const SkillModal = ({ addSkill, modalOpen, setOpenModal }) => (
-  <Modal show={modalOpen} onHide={() => setOpenModal(false)}>
-    <Modal.Header>
-      <Modal.Title>{addSkill ? 'Add New Skill' : 'Edit Skill'}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <Form>
-        <Form.Group
-          controlId={addSkill ? 'formAddSkillName' : 'formEditSkillName'}
-        >
-          <Form.Label>Skill Name</Form.Label>
-          <Form.Control type="text" placeholder="Skill" />
-        </Form.Group>
-        <Form.Group
-          controlId={
-            addSkill ? 'formAddSkillDescription' : 'formEditSkillDescription'
-          }
-        >
-          <Form.Label>Skill Description</Form.Label>
-          <Form.Control as="textarea" rows="3" />
-        </Form.Group>
-      </Form>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="outline-secondary" onClick={() => setOpenModal(false)}>
-        Cancel
-      </Button>
-      <Button variant="primary" onClick={() => setOpenModal(false)}>
-        Save Changes
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+const SkillModal = ({
+  addSkill,
+  modalOpen,
+  setOpenModal,
+  clickedModalData,
+  setClickedModalData,
+}) => {
+  const [values, setValues] = useState(clickedModalData);
 
-const DeleteModal = ({ modalOpen, setOpenModal }) => (
-  <Modal show={modalOpen} onHide={() => setOpenModal(false)}>
-    <Modal.Header>
-      <Modal.Title>Please confirm</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>Are you sure you want to delete this skill?</Modal.Body>
-    <Modal.Footer>
-      <Button variant="outline-secondary" onClick={() => setOpenModal(false)}>
-        Cancel
-      </Button>
-      <Button variant="danger" onClick={() => setOpenModal(false)}>
-        Delete
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+  useEffect(() => {
+    setValues(clickedModalData);
+  }, [clickedModalData]);
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setClickedModalData(null);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('validate and add to database with loading spinner');
+    console.log(`submitting: values: ${JSON.stringify(values)}`);
+    handleCloseModal();
+  };
+
+  console.log(
+    `addSkill: ${addSkill}, modalOpen: ${modalOpen}, values: ${JSON.stringify(
+      values,
+    )}`,
+  );
+
+  return (
+    <Modal show={modalOpen} onHide={handleCloseModal}>
+      {values && (
+        <>
+          <Modal.Header>
+            <Modal.Title>
+              {addSkill ? 'Add New Skill' : 'Edit Skill'}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form id="skills-list-form" onSubmit={handleSubmit}>
+              <Form.Group
+                controlId={addSkill ? 'formAddSkillName' : 'formEditSkillName'}
+              >
+                <Form.Label>Skill Name</Form.Label>
+                <Form.Control
+                  name="skillName"
+                  type="text"
+                  value={values.skillName}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group
+                controlId={
+                  addSkill
+                    ? 'formAddSkillDescription'
+                    : 'formEditSkillDescription'
+                }
+              >
+                <Form.Label>Skill Description</Form.Label>
+                <Form.Control
+                  name="skillDescription"
+                  as="textarea"
+                  rows="3"
+                  value={values.skillDescription}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-secondary" onClick={handleCloseModal}>
+              Cancel
+            </Button>
+            <Button type="submit" form="skills-list-form" variant="primary">
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </>
+      )}
+    </Modal>
+  );
+};
+
+const DeleteModal = ({
+  modalOpen,
+  setOpenModal,
+  clickedModalData,
+  setClickedModalData,
+}) => {
+  const handleCloseModal = () => {
+    setClickedModalData(null);
+    setOpenModal(false);
+  };
+
+  const handleDelete = () => {
+    console.log('delete from database with loading spinner');
+    console.log(`deleting: values: ${JSON.stringify(clickedModalData)}`);
+    handleCloseModal();
+  };
+
+  console.log(
+    `deleteModal, modalOpen: ${modalOpen}, values: ${JSON.stringify(
+      clickedModalData,
+    )}`,
+  );
+
+  return (
+    <Modal show={modalOpen} onHide={handleCloseModal}>
+      {clickedModalData && (
+        <>
+          <Modal.Header>
+            <Modal.Title>Please confirm</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete the skill,{' '}
+            {clickedModalData.skillName}?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-secondary" onClick={handleCloseModal}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleDelete}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </>
+      )}
+    </Modal>
+  );
+};
 
 const EditSkills = () => {
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [addSkillModalOpen, setAddSkillModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [clickedModalData, setClickedModalData] = useState(null);
 
   const [data, setData] = useState({ Items: [] });
-
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(null);
 
@@ -114,7 +201,14 @@ const EditSkills = () => {
       <Button
         disabled={isLoading}
         css="margin-bottom: 40px"
-        onClick={() => setAddSkillModalOpen(true)}
+        onClick={() => {
+          setClickedModalData({
+            skillName: '',
+            skillDescription: '',
+            skillId: '',
+          });
+          setAddSkillModalOpen(true);
+        }}
       >
         Add new skill
       </Button>
@@ -122,77 +216,119 @@ const EditSkills = () => {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <StyledSkillsGrid>
-          {data.Items.map(skill => {
-            const { skillName, skillDescription } = skill;
-            return (
-              <Card key={skillName}>
-                <Card.Body>
-                  <Card.Title>
-                    <div css="float: right">
-                      <OverlayTrigger
-                        placement="top"
-                        overlay={<Tooltip>Edit</Tooltip>}
-                      >
-                        <StyledButton
-                          name={skillName}
-                          type="button"
-                          onClick={() => setEditModalOpen(true)}
+        <>
+          <StyledSkillsGrid>
+            {data.Items.map((skill, index) => {
+              const { skillName, skillDescription } = skill;
+              return (
+                <Card key={skillName}>
+                  <Card.Body>
+                    <Card.Title>
+                      <div css="float: right">
+                        <OverlayTrigger
+                          placement="top"
+                          overlay={<Tooltip>Edit</Tooltip>}
                         >
-                          <Edit size={18} />
-                        </StyledButton>
-                      </OverlayTrigger>
-                      <OverlayTrigger
-                        placement="top"
-                        overlay={<Tooltip>Delete</Tooltip>}
-                      >
-                        <StyledButton
-                          type="button"
-                          onClick={() => setDeleteModalOpen(true)}
+                          <StyledButton
+                            name={skillName}
+                            type="button"
+                            onClick={() => {
+                              setClickedModalData(data.Items[index]);
+                              setEditModalOpen(true);
+                            }}
+                          >
+                            <Edit size={18} />
+                          </StyledButton>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                          placement="top"
+                          overlay={<Tooltip>Delete</Tooltip>}
                         >
-                          <Trash2 size={18} />
-                        </StyledButton>
-                      </OverlayTrigger>
-                    </div>
-                    {skillName}
-                  </Card.Title>
-                  <Card.Text>{skillDescription}</Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </StyledSkillsGrid>
+                          <StyledButton
+                            type="button"
+                            onClick={() => {
+                              setClickedModalData(data.Items[index]);
+                              setDeleteModalOpen(true);
+                            }}
+                          >
+                            <Trash2 size={18} />
+                          </StyledButton>
+                        </OverlayTrigger>
+                      </div>
+                      {skillName}
+                    </Card.Title>
+                    <Card.Text>{skillDescription}</Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </StyledSkillsGrid>
+
+          <SkillModal
+            addSkill
+            modalOpen={addSkillModalOpen}
+            setOpenModal={setAddSkillModalOpen}
+            clickedModalData={clickedModalData}
+            setClickedModalData={setClickedModalData}
+          />
+
+          <SkillModal
+            modalOpen={editModalOpen}
+            setOpenModal={setEditModalOpen}
+            clickedModalData={clickedModalData}
+            setClickedModalData={setClickedModalData}
+          />
+
+          <DeleteModal
+            modalOpen={deleteModalOpen}
+            setOpenModal={setDeleteModalOpen}
+            clickedModalData={clickedModalData}
+            setClickedModalData={setClickedModalData}
+          />
+        </>
       )}
-
-      <DeleteModal
-        modalOpen={deleteModalOpen}
-        setOpenModal={setDeleteModalOpen}
-      />
-
-      <SkillModal
-        addSkill
-        modalOpen={addSkillModalOpen}
-        setOpenModal={setAddSkillModalOpen}
-      />
-
-      <SkillModal modalOpen={editModalOpen} setOpenModal={setEditModalOpen} />
     </StyledMain>
   );
 };
 
 SkillModal.defaultProps = {
   addSkill: false,
+  clickedModalData: {
+    skillName: '',
+    skillDescription: '',
+    skillId: '',
+  },
+};
+
+DeleteModal.defaultProps = {
+  clickedModalData: {
+    skillName: '',
+    skillDescription: '',
+    skillId: '',
+  },
 };
 
 SkillModal.propTypes = {
   addSkill: PropTypes.bool,
   modalOpen: PropTypes.bool.isRequired,
   setOpenModal: PropTypes.func.isRequired,
+  clickedModalData: PropTypes.shape({
+    skillName: PropTypes.string,
+    skillDescription: PropTypes.string,
+    skillId: PropTypes.string,
+  }),
+  setClickedModalData: PropTypes.func.isRequired,
 };
 
 DeleteModal.propTypes = {
   modalOpen: PropTypes.bool.isRequired,
   setOpenModal: PropTypes.func.isRequired,
+  clickedModalData: PropTypes.shape({
+    skillName: PropTypes.string,
+    skillDescription: PropTypes.string,
+    skillId: PropTypes.string,
+  }),
+  setClickedModalData: PropTypes.func.isRequired,
 };
 
 export default EditSkills;
