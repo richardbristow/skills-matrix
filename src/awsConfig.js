32bcec0f1e3dd7db872d23d0b5f8f7065e4f4277
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-08-11 18:31:51 +0100

    Added serverless deploy to CI workflow

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-const amplifyConfig = {
+const devConfig = {
   Auth: {
     mandatorySignIn: true,
     identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
@@ -12,12 +12,34 @@ const amplifyConfig = {
     endpoints: [
       {
         name: 'skillsMatrix',
-        endpoint: process.env.REACT_APP_API_URL,
+        endpoint: process.env.REACT_APP_DEV_API_URL,
         region: process.env.REACT_APP_AWS_REGION,
       },
     ],
   },
 };
+
+const prodConfig = {
+  Auth: {
+    mandatorySignIn: true,
+    identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+    region: process.env.REACT_APP_AWS_REGION,
+    userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_COGNITO_USER_POOL_WEB_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'skillsMatrix',
+        endpoint: process.env.REACT_APP_PROD_API_URL,
+        region: process.env.REACT_APP_AWS_REGION,
+      },
+    ],
+  },
+};
+
+const amplifyConfig =
+  process.env.REACT_APP_STAGE === 'prod' ? prodConfig : devConfig;
 
 AWS.config.update({
   region: 'us-east-1',
