81c2a96b97ef71a5b26b9af8b66d976b22524d07
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-08-28 13:55:25 +0100

    Adding padding-bottom to main h2 headers

--- src/shared/StyledMain.jsx
+++ src/shared/StyledMain.jsx
@@ -4,6 +4,9 @@ const StyledMain = styled.div`
   grid-area: main;
   padding: 60px 20px 20px 20px;
   margin: ${({ centre }) => (centre ? '20px auto' : '20px')};
+  h2:first-of-type {
+    padding-bottom: 20px;
+  }
 `;
 
 export default StyledMain;
