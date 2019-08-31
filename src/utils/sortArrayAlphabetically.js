68e718582b1394cdcedf1e1d48715f8b2048061a
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-08-31 15:32:41 +0100

    Added sortArrayAlphabetically util function

---
+++ src/utils/sortArrayAlphabetically.js
@@ -0,0 +1,15 @@
+const sortArrayAlphabetically = array => {
+  return array.sort((a, b) => {
+    const aLower = a.toLowerCase();
+    const bLower = b.toLowerCase();
+    if (aLower < bLower) {
+      return -1;
+    }
+    if (aLower < bLower) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+export default sortArrayAlphabetically;
