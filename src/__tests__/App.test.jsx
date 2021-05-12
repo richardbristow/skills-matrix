5659999d2bd90ee75d1586206b4d0c83183da592
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-12 12:30:03 +0100

    updated tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { waitForDomChange, getByText } from '@testing-library/react';
+import { getByText, waitFor } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../../testFunctions/renderWithRouter';
 
 describe('App', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
   it('renders without throwing errors', async () => {
     const { container } = renderWithRouter(<App />);
-    await waitForDomChange();
-    expect(
-      getByText(container, 'Login', { selector: 'h3' }),
-    ).toBeInTheDocument();
+    // console.error = jest.fn();
+    await waitFor(() => {
+      expect(
+        getByText(container, 'Login', { selector: 'h3' }),
+      ).toBeInTheDocument();
+    });
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/__tests__/NoRoute.test.jsx
+++ src/components/__tests__/NoRoute.test.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import {
-  getByText,
-  queryByText,
-  waitForDomChange,
-} from '@testing-library/react';
+import { getByText, queryByText, waitFor } from '@testing-library/react';
 import App from '../../App';
 import renderWithRouter from '../../../testFunctions/renderWithRouter';
 
 describe('NoRoute', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
   it('should render the NoRoute component without errors on non matching route', async () => {
     const { container } = renderWithRouter(<App />, {
       route: '/non-matching-route',
     });
-    await waitForDomChange();
-    expect(getByText(container, '404', { selector: 'h1' })).toBeInTheDocument();
-    expect(getByText(container, 'Page does not exist.')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(
+        getByText(container, '404', { selector: 'h1' }),
+      ).toBeInTheDocument();
+      expect(getByText(container, 'Page does not exist.')).toBeInTheDocument();
+    });
     expect(container).toMatchSnapshot();
   });
 
@@ -22,15 +25,16 @@ describe('NoRoute', () => {
     const { container } = renderWithRouter(<App />, {
       route: '/login',
     });
-    await waitForDomChange();
-    expect(
-      queryByText(container, '404', { selector: 'h1' }),
-    ).not.toBeInTheDocument();
-    expect(
-      queryByText(container, 'Page does not exist.'),
-    ).not.toBeInTheDocument();
-    expect(
-      getByText(container, 'Login', { selector: 'h3' }),
-    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(
+        queryByText(container, '404', { selector: 'h1' }),
+      ).not.toBeInTheDocument();
+      expect(
+        queryByText(container, 'Page does not exist.'),
+      ).not.toBeInTheDocument();
+      expect(
+        getByText(container, 'Login', { selector: 'h3' }),
+      ).toBeInTheDocument();
+    });
   });
 });
